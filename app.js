b7ecc99c5d3e7862bb35d845bba4b11a64727974
cmbandali <cmbandali>
2018-11-25 15:05:41 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+
+const server_address = 'localhost';
+const port = 3000;
+
+let html_stream = fs.createReadStream('./assets/index.html', 'utf8');
+let search_stream = fs.createReadStream('./html/search-form.html');
+
+let server = http.createServer((req,res)=>{
+  if(req.url === '/'){
+    console.log(`A new request was made from ${req.connection.remoteAddress} for ${req.url}`);
+    res.writeHead(200,{'Content-Type':'text/html'});
+    html_stream.pipe(res);
+  } else if(req.url.includes('/favicon.ico')){
+    console.log(`A new favicon request was made from ${req.connection.remoteAddress} for ${req.url}`);
+    res.writeHead(404);
+    res.end();
+  } else if(req.url.includes('/artists/')){
+    console.log(`A new artist request was made from ${req.connection.remoteAddress} for ${req.url}`);
+    res.writeHead(200,{'Content-Type':'text/html'});
+    html_stream.pipe(res);
+  } else if( req.url.includes('/search')){
+    console.log(`A new search request was made from ${req.connection.remoteAddress} for ${req.url}`);
+    res.writeHead(200,{'Content-Type':'text/html'});
+    search_stream.pipe(res);
+  }
+});
+
+console.log('Now listening on port ' + port);
+server.listen(port, server_address);
