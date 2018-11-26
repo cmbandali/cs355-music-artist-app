7c69662fb7d303ff4dae7e63b566459719d7ec11
cmbandali <cmbandali3@gmail.com>
2018-11-26 12:38:58 -0500

    deleted assets folder and changed default stream to search_stream in app.js

--- app.js
+++ app.js
@@ -4,21 +4,21 @@ const fs = require('fs');
 const server_address = 'localhost';
 const port = 3000;
 
-let html_stream = fs.createReadStream('./assets/index.html', 'utf8');
+// let html_stream = fs.createReadStream('./assets/index.html', 'utf8');
 let search_stream = fs.createReadStream('./html/search-form.html');
 
 let server = http.createServer((req,res)=>{
   if(req.url === '/'){
     console.log(`A new request was made from ${req.connection.remoteAddress} for ${req.url}`);
     res.writeHead(200,{'Content-Type':'text/html'});
-    html_stream.pipe(res);
+    search_stream.pipe(res);
   } else if(req.url.includes('/favicon.ico')){
     console.log(`A new favicon request was made from ${req.connection.remoteAddress} for ${req.url}`);
     res.writeHead(404);
     res.end();
   } else if(req.url.includes('/artists/')){
     console.log(`A new artist request was made from ${req.connection.remoteAddress} for ${req.url}`);
-    res.writeHead(200,{'Content-Type':'text/html'});
+    res.writeHead(200,{'Content-Type':'image/jpeg'});
     html_stream.pipe(res);
   } else if( req.url.includes('/search')){
     console.log(`A new search request was made from ${req.connection.remoteAddress} for ${req.url}`);
@@ -29,3 +29,9 @@ let server = http.createServer((req,res)=>{
 
 console.log('Now listening on port ' + port);
 server.listen(port, server_address);
+
+// image_stream.on('error', function(err) {
+//   console.log(err);
+//   res.writeHead(404);
+//   return res.end();
+// });
