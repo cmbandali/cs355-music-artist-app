b92409f4bc2849a3456de6ab5cde266765cfeae5
cmbandali <cmbandali3@gmail.com>
2018-12-02 23:04:34 -0500

    Got around nodejs bug, working code

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const port = 3000;
 let search_stream = fs.createReadStream('./html/search-form.html');
 
 let server = http.createServer((req,res)=>{
+  
   res.writeHead(200,{'Content-Type':'text/html'});
-  search_stream.pipe(res);
 
   if(req.url === '/'){
 
@@ -33,23 +33,32 @@ let server = http.createServer((req,res)=>{
 
     console.log(`A new artist request was made from ${req.connection.remoteAddress} for ${req.url}`);
     res.writeHead(200,{'Content-Type':'image/jpeg'});
-    html_stream.pipe(res);
-
+    // html_stream.pipe(res);
+    // image_stream.on('error', function(err) {
+    //   console.log(err);
+    //   res.writeHead(404);
+    //   return res.end();
+    // })
   } else if( req.url.includes('/search')){
-
     console.log(`A new search request was made from ${req.connection.remoteAddress} for ${req.url}`);
+    search_stream.pipe(res);
     const querystring = require('querystring');
+    /*user input converted to an object*/
     let user_input = req.url;
-    let name = querystring.parse(user_input, "/search?");
+    user_input = querystring.parse(user_input, "/search?"); //{ artist: 'Drake' }
 
+    /*Spotify identification - going to be using client credentials flow*/
     let post_data = {
       client_id: credentials.client_id,
       client_secret: credentials.client_secret,
       grant_type: "client_credentials",
     }
-    post_data = querystring.stringify(post_data);
+    post_data = querystring.stringify(post_data); //data must be sent as a query string
 
+    /*tells method being used and header information*/
     let options = {
+      host: 'accounts.spotify.com',
+      path: '/api/token',
       method:'POST',
       headers:{
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -57,34 +66,70 @@ let server = http.createServer((req,res)=>{
       }
     }
 
-    /*Create the request using http.request and supplying it relevant data*/
     const authentication_req_url = 'https://accounts.spotify.com/api/token';
-    let request_sent_time = new Date();
-    let authentication_req = https.request(authentication_req_url, options, authentication_res => {
-      recieved_authentication(authentication_res,res,user_input,request_sent_time);
-    });
-    authentication_req.on('error', (e) => {
-      console.log(e);
-    });
-    authentication_req.write(post_data);
-    console.log("Requesting Token");
-    authentication_req.end();
+    // let request_sent_time = new Date();
 
-    /*function to catch the result of the authentication request*/
+    /*Create the request using https.request and supplying it relevant data*/
+    // let authentication_req = https.request(options, (authentication_res) => {
+    //   recieved_authentication(authentication_res,res,user_input,request_sent_time); /*Callback function to be triggered when we recieve a response*/
+    // });
+
+    // authentication_req.on('error', (e) => {
+    //   console.log(e);
+    // });
+
+    // /*write function sends request and puts post_data into the body of the request since method is POST*/
+    // authentication_req.write(post_data);
+    // console.log("Requesting Token");
+    // authentication_req.end();
+
+    /*Check if cached access_token exists and if it is expired*/
+    let cache_valid = false;
+    let authentication_cache = './auth/authentication_res.json';
+    if(fs.existsSync(authentication_cache)) {
+      content = fs.readFileSync(authentication_cache, 'utf-8');
+      cached_auth = JSON.parse(content); //authentication json convertes to js object
+      if(cached_auth.expiration > Date.now()) cache_valid = true; //access_token is still valid
+      else console.log('Access_Token is Expired, need to request new token');
+    }
+
+    if(cache_valid) create_search_req(cached_auth, res, user_input);
+    else {
+      let request_sent_time = new Date();
+      /*Create the request using https.request and supplying it relevant data*/
+      let authentication_req = https.request(options, (authentication_res) => {
+        recieved_authentication(authentication_res,res,user_input,request_sent_time); /*Callback function to be triggered when we recieve a response*/
+      });
+
+      authentication_req.on('error', (e) => {
+        console.log(e);
+      });
+
+      /*write function sends request and puts post_data into the body of the request since method is POST*/
+      authentication_req.write(post_data);
+      console.log("Requesting Token");
+      authentication_req.end();
+    }
+
+    /*Callback function catches the result of the authentication request*/
     function recieved_authentication(authentication_res,res,user_input,request_sent_time){
       authentication_res.setEncoding("utf8");
       let body = "";
       authentication_res.on("data", data => {body += data;});
       authentication_res.on("end", () => {
-        let authentication_res_data = JSON.parse(body);
-        authentication_res_data.expiration =  new Date().setHours(request_sent_time.getHours() + 1);
+        let authentication_res_data = JSON.parse(body); //convers back to a JS object
+
+        let date = new Date().setHours(request_sent_time.getHours() + 1);
+        let JSONdate = JSON.stringify(date);
+        authentication_res_data.expiration = JSONdate;
+
         console.log(authentication_res_data);
         create_cache(authentication_res_data);
-        // create_search_req(authentication_res_data,res,user_input,request_sent_time);
+        create_search_req(authentication_res_data,res,user_input/*,request_sent_time*/);
       });
     }
 
-    /*writes to a file*/
+    /*writes to cache (aka authentication_res.json)*/
     function create_cache(authentication_res_data){
       data = JSON.stringify(authentication_res_data);
       fs.writeFile('./auth/authentication_res.json', data, (err) => {
@@ -93,78 +138,18 @@ let server = http.createServer((req,res)=>{
       });
     }
 
-    /*Checking to see if a cached file exists*/
-    let cache_valid = false;
-    const authentication_cache = './auth/authentication_res.json';
-    if(fs.existsSync(authentication_cache)) {
-      content = fs.readFileSync(authentication_cache, 'utf-8');
-      cached_auth = JSON.parse(content);
-      if(new Date(cached_auth.expiration) > Date.now()){
-        cache_valid = true;
-      }
-      else{
-        console.log('Token Expired');
-      }
-    }
-    if(cache_valid){
-      // create_search_req(cached_auth,res,user_input);
-    }
-    else{
-      const authentication_req_url = 'https://accounts.spotify.com/api/token';
-      let request_sent_time = new Date();
-      let authentication_req = https.request(authentication_req_url, options, authentication_res => {
-        recieved_authentication(authentication_res,res,user_input,request_sent_time);
-      });
-      authentication_req.on('error', (e) => {
-        console.errors(e);
-      });
-      authentication_req.write(post_data);
-      console.log("Requesting Token");
-      authentication_req.end();
+    /*start the search request*/
+    function create_search_req(cached_auth, res, user_input) {
+
+      console.log(`searching for ${user_input.artist}...`);
+      let access_token = cached_auth.access_token;
+      let spotify_url = https://api.spotify.com/v1/search;
     }
 
-    /*Request to download the image at the url specfied*/
-    
-    /*Stream to get access_token*/
-    const authentication_json = fs.readFileSync('./auth/authentication_res.json', 'utf-8');
-    const authentication = JSON.parse(authentication_json);
+    /*!!remove search_stream.pipe(res); in beginning when you stream artist page*/
+  } //else if
 
-    let url_data = {
-      q: name.artist,
-      type: 'artist',
-      access_token: authentication.access_token
-    }
-    url_data = querystring.stringify(url_data);
-    let url = 'https://api.spotify.com/v1/search?' + url_data;
-    console.log(url);
-
-    let img_path = `./artists/${name.artist}.jpg`
-
-    let image_req = https.get(url, image_res => {
-      console.log("response: " + image_res.statusCode);
-      let new_img = fs.createWriteStream(img_path,{'encoding':null});
-      image_res.pipe(new_img);
-
-      // new_img.on("data", function(chunk) {
-      //   console.log("BODY: " + chunk);
-      // });
-
-      new_img.on('finish', function() {
-        // let img_data = JSON.stringify(image_res.headers);
-        // let img_res_data = JSON.parse(body);
-        // create_cache(img_data, './artists/{name.artist}.json');
-        let webpage = `<h1>${name.artist}</h1><p>Music</p><img src="./artists/${name.artist}.jpg" />`;
-        // webpage.pipe(res);
-      });
-
-    });
-    image_req.on('error', (e) => {
-      console.log(e);
-    });
-
-  }//search else if
-
-});
+}); //server
 
 console.log('Now listening on port ' + port);
 server.listen(port, server_address);
